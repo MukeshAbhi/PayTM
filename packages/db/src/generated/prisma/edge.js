976deb9c65b8de5c021cd5b001ea66f767475973
MukeshAbhi <abhimukesh284@gmail.com>
2025-05-26 22:56:35 +0530

    addind CI/CD

--- packages/db/src/generated/prisma/edge.js
+++ packages/db/src/generated/prisma/edge.js
@@ -256,11 +256,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:MyPassword@localhost:5432/postgres"
+        "value": null
       }
     }
   },
