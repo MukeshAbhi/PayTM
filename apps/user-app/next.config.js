d1ccfdce18a6c8ef2991c792d5826be0266e3a9e
MukeshAbhi <abhimukesh284@gmail.com>
2025-07-05 12:02:18 +0530

    working

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ["@repo/ui"],
-    images: {
-      domains: ["avatars.githubusercontent.com"],
-    },
-    // Netlify deployment configuration
-    trailingSlash: false,
-  }
+  transpilePackages: ["@repo/ui"],
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
+  trailingSlash: false,
+  webpack: async (config, { isServer }) => {
+    if (isServer) {
+      const { PrismaPlugin } = await import('@prisma/nextjs-monorepo-workaround-plugin');
+      config.plugins.push(new PrismaPlugin());
+    }
+    return config;
+  },
+};
 
-  export default nextConfig
-  
+export default nextConfig;
