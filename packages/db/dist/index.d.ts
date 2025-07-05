696c3af92bc7adb77d65128d05830a88f6e5ddcc
MukeshAbhi <abhimukesh284@gmail.com>
2025-07-05 11:53:11 +0530

    r

---
+++ packages/db/dist/index.d.ts
@@ -0,0 +1,5 @@
+import { PrismaClient, Prisma, $Enums } from "./generated/prisma/index.js";
+export declare const prisma: PrismaClient<Prisma.PrismaClientOptions, never, import("./generated/prisma/runtime/library.js").DefaultArgs>;
+export { Prisma, $Enums };
+export declare const adapter: any;
+//# sourceMappingURL=index.d.ts.map
