5afd0062f887c568fec029d70d7a3be56f28388f
MukeshAbhi <abhimukesh284@gmail.com>
2025-07-05 12:04:28 +0530

    working

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,3 +1,5 @@
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
@@ -5,9 +7,8 @@ const nextConfig = {
     domains: ["avatars.githubusercontent.com"],
   },
   trailingSlash: false,
-  webpack: async (config, { isServer }) => {
+  webpack: (config, { isServer }) => {
     if (isServer) {
-      const { PrismaPlugin } = await import('@prisma/nextjs-monorepo-workaround-plugin');
       config.plugins.push(new PrismaPlugin());
     }
     return config;
