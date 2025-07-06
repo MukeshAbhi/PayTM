c9f466373132f11fef11445825193f0f8e08576e
MukeshAbhi <abhimukesh284@gmail.com>
2025-07-07 00:58:47 +0530

    working

--- packages/db/postinstall.sh
+++ packages/db/postinstall.sh
@@ -1,4 +1,5 @@
 echo "🔧 Running Prisma generate..."
+echo "👀 PRISMA_CLI_BINARY_TARGETS = $PRISMA_CLI_BINARY_TARGETS"
 pnpm db:generate
 
 echo "📦 Trying to copy the Prisma query engine for rhel-openssl-3.0.x..."
