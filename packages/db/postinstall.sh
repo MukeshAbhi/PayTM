e63f31032788fe04230adafbd6681218f355ee16
MukeshAbhi <abhimukesh284@gmail.com>
2025-07-07 02:40:29 +0530

    working

--- packages/db/postinstall.sh
+++ packages/db/postinstall.sh
@@ -1,7 +1,16 @@
 echo "🔧 Running Prisma generate..."
 echo "👀 PRISMA_CLI_BINARY_TARGETS = $PRISMA_CLI_BINARY_TARGETS"
+
+# Generate Prisma Client
 pnpm db:generate
 
-echo "📦 Trying to copy the Prisma query engine for rhel-openssl-3.0.x..."
+# Get the expected binary path for the deployed runtime
+PRISMA_BINARY_TARGET=${PRISMA_CLI_BINARY_TARGETS:-rhel-openssl-3.0.x}
+PRISMA_ENGINE_SOURCE_PATH="./node_modules/@prisma/engines/query-engine-${PRISMA_BINARY_TARGET}.so.node"
+PRISMA_ENGINE_DEST_PATH="./src/generated/prisma/libquery_engine-${PRISMA_BINARY_TARGET}.so.node"
 
-cp -r node_modules/.prisma/client node_modules/@prisma/client/
+# Copy the correct engine binary
+echo "📦 Copying Prisma query engine to: $PRISMA_ENGINE_DEST_PATH"
+mkdir -p ./src/generated/prisma
+cp "$PRISMA_ENGINE_SOURCE_PATH" "$PRISMA_ENGINE_DEST_PATH" || echo "❌ Failed to copy Prisma engine binary"
+
