cc151fda86578d21011dbcc75467ff285f9de3e6
MukeshAbhi <abhimukesh284@gmail.com>
2025-07-11 11:23:25 +0530

    working

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,12 +2,12 @@
 
 import { Intro } from "@/components/Intro";
 import { useSession } from "next-auth/react";
-import { redirect } from 'next/navigation';
+//import { redirect } from 'next/navigation';
 
 export default function Page() {
   const { data: session } = useSession();
   if (session?.user) {
-    redirect('/dashboard/home')
+    //redirect('/dashboard/home')
   } else {
     return (
       <Intro/>
