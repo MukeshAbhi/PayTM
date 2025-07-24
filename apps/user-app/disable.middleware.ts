7bcc5d983d447d0be72b174ce27b676bbccf2b09
MukeshAbhi <abhimukesh284@gmail.com>
2025-07-24 17:52:34 +0530

    Bug

---
+++ apps/user-app/disable.middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+const protectedRoutes = ["/user","/dashboard"];
+
+export const middleware = async (request:NextRequest) =>  {
+    const { pathname } = request.nextUrl;
+
+    // Skip middleware for auth API routes and signin page
+    if (pathname.startsWith('/api/auth') || pathname === '/signin') {
+        return NextResponse.next();
+    }
+
+    const token = await getToken({req: request , secret: process.env.NEXTAUTH_SECRET});
+
+    const isProtected = protectedRoutes.some((route) =>
+        pathname.startsWith(route)
+    );
+
+    if(isProtected && !token) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    return NextResponse.next();
+
+}
