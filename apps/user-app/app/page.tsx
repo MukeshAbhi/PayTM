1fce250ab68e3693b7797dddf2c7bbd57f494ab5
MukeshAbhi <abhimukesh284@gmail.com>
2025-07-24 17:31:37 +0530

    Bug

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,18 +1,13 @@
 import { auth } from "@/authTypes";
 import { Intro } from "@/components/Intro";
-import { redirect } from 'next/navigation';
+
 
 export default async function Page() {
   const session = await auth();
   console.log("Form home : ", session);
   
-  if (session?.user) {
-    redirect('/dashboard/home')
-  } else {
-    return (
+  return (
       <Intro/>
     )
-  }
-    
 }
 
