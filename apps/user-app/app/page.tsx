26dc1e4e4990e8f49ce023b2256b81c869060503
MukeshAbhi <abhimukesh284@gmail.com>
2025-07-24 17:00:46 +0530

    solved a  bug

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,11 +1,11 @@
-"use client";
-
+import { auth } from "@/authTypes";
 import { Intro } from "@/components/Intro";
-import { useSession } from "next-auth/react";
 import { redirect } from 'next/navigation';
 
-export default function Page() {
-  const { data: session } = useSession();
+export default async function Page() {
+  const session = await auth();
+  console.log("Form home : ", session);
+  
   if (session?.user) {
     redirect('/dashboard/home')
   } else {
