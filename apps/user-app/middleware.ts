24f9fcab5b15df7c36e2f77fa707975e89367953
MukeshAbhi <abhimukesh284@gmail.com>
2025-08-05 19:50:59 +0530

    working

--- apps/user-app/middleware.ts
+++ apps/user-app/middleware.ts
@@ -1,34 +1,21 @@
+// middleware.ts
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
 
-const protectedRoutes = ["/user","/dashboard"];
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
 
-// export function middleware(req: NextRequest) {
-//   return NextResponse.next(); // 🔁 do nothing, just pass through
-// }
+  console.log("Token in middleware:", token);
 
-export const middleware = async (request:NextRequest) =>  {
-    const { pathname } = request.nextUrl;
-    
-    
-    // Skip middleware for auth API routes and signin page
-    if (pathname.startsWith('/api/auth') || pathname === '/signin' || pathname.startsWith('/o/oauth2/v2')) {
-        return NextResponse.next();
-    }
+  if (!token) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
 
-    const token = await getToken({req: request , secret: process.env.NEXTAUTH_SECRET});
-    console.log("token name: ", token);
-
-    const isProtected = protectedRoutes.some((route) =>
-        pathname.startsWith(route)
-    );
-
-    if(isProtected && !token) {
-        return NextResponse.redirect(new URL("/", request.url));
-    }
-
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
